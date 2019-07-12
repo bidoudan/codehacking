6353136752c3ae4b9c840b4d63e590fd89d42709
bidoudan <quran.fr09@gmail.com>
2019-07-12 16:44:23 +0100

    able to create posts with dynamic categories

--- app/Post.php
+++ app/Post.php
@@ -22,4 +22,10 @@ class Post extends Model
     public function photo(){
         return $this->belongsTo('App\Photo');
     }
+
+    public function category(){
+
+        return $this->belongsTo('App\Category');
+
+    }
 }
